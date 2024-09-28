03d8cd17fdb94faf10d7a5a7d5a30811effcf657
jlara <jlaramintic@gmail.com>
2024-09-27 19:56:08 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import morgan from "morgan";
+
+
+import indexRoutes from "./routes/index.routes.js";
+import adquirientesRoutes from "./routes/adquirientes.routes.js";
+import cors from 'cors'; // Importar cors usando ES Modules
+
+const app = express();
+
+// Configuración de CORS
+const corsOptions = {
+  //origin: 'http://localhost:3000', // Reemplazar con el origen de la aplicación
+  origin: '*', // Permitir cualquier origen v1
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions)); // Usar cors con las opciones configuradas
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Routes
+app.use("/", indexRoutes);
+
+app.use("/api", adquirientesRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+export default app;
